26ecb08b539f08bd039aba037acbe8bea1e6ecc8
inkinno <inno.inkin@gmail.com>
2026-06-07 13:35:32 +0900

    Add GitHub Actions for Pages deployment and fix l10n imports

---
+++ test/viewer_test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+import 'package:viewer_web/main.dart';
+import 'package:viewer_web/controllers/timeline_controller.dart';
+import 'package:viewer_web/screens/viewer_screen.dart';
+
+void main() {
+  testWidgets('ViewerScreen renders without error', (WidgetTester tester) async {
+    FlutterError.onError = (FlutterErrorDetails details) {
+      print('FlutterError: ' + details.exception.toString());
+      print(details.stack);
+    };
+
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_) => TimelineController()),
+        ],
+        child: const MyApp(),
+      ),
+    );
+
+    await tester.pumpAndSettle();
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+}
